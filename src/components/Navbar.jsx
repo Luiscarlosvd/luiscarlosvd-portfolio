bb947434186ebcd74ea13114d992665e048a41f5
Luiscarlosvd <lc26cr7@gmail.com>
2023-10-14 12:06:10 -0400

    Fix navbar styles for mobile design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import lvicon from '../assets/Luis-Valera.png';
 
 const Navbar = () => {
     return (
-      <header className='relative flex flex-col p-2 items-center md:flex-row md:px-40 md:py-4'>
+      <header className='relative flex flex-col p-2 items-center shadow-md md:flex-row md:px-40 md:py-4'>
         <div className='md:flex-1 md:justify-start'>
           <img src={lvicon} alt="Luis Valera Icon" className='w-60 py-2 md:w-48'></img>
         </div>
         <nav className="text-lg flex flex-col items-center py-2 gap-4 text-midnight-green md:flex-row md:self-center md:shrink-0">
-            <NavLink className="font-semibold">Home</NavLink>|
-            <NavLink className="font-semibold">Projects</NavLink>|
-            <NavLink className="font-semibold ">About me</NavLink>|
+            <NavLink className="font-semibold">Home</NavLink>
+            <NavLink className="font-semibold">Projects</NavLink>
+            <NavLink className="font-semibold ">About me</NavLink>
             <NavLink className="font-semibold">Contact</NavLink>
         </nav>
         <div className='flex md:flex-1 md:justify-end'>

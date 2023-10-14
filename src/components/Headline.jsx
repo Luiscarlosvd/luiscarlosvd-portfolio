8d3de8a3a80b37239486095e954436f9592b5ffe
Luiscarlosvd <lc26cr7@gmail.com>
2023-10-14 12:06:30 -0400

    Add basic headline section with description

---
+++ src/components/Headline.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Headline = () => {
+  return (
+    <section className='py-10 px-11'>
+      <div>
+        <h1 className='text-shadow font-bold text-3xl text-dark-spring-green'><span className='text-midnight-green'>Full-stack</span> Web Developer</h1>
+        <br />
+        <p className='text-lg text-gray-500'>
+          Hello! I'm <span className='text-midnight-green'>Luis Carlos Valera</span>, a passionate Full Stack Web Developer 
+          focused on continuous learning. I have experience in front-end and back-end 
+          development, using technologies such as JavaScript, React and Ruby on 
+          Rails. I'm excited to connect with you. Plus, I'm a proud fan of <span className='text-midnight-green'>Real Madrid!</span>
+        </p>
+        <br />
+        <h2 className='text-lg font-semibold text-zomp text-shadow'>Let's talk and explore new possibilities in the world of web development!</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Headline

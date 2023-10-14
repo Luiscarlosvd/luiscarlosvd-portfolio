30059a6e18e849175a27d187723d14383690288e
Luiscarlosvd <lc26cr7@gmail.com>
2023-10-14 13:11:08 -0400

    Create component for Projects List

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import Headline from './Headline';
 import Navbar from './Navbar';
+import ProjectList from './ProjectList';
 
 const Layout = () => (
   <div>
     <Navbar />
     <Headline />
+    <ProjectList />
   </div>
 );
 export default Layout;
---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,10 @@
+
+const ProjectList = () => {
+  return (
+    <div>
+      <h1>My Projects</h1>
+    </div>
+  )
+}
+
+export default ProjectList

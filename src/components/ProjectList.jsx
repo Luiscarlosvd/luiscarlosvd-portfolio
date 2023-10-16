28b183e5973e76d892daebc1236567159533939f
Luiscarlosvd <lc26cr7@gmail.com>
2023-10-16 12:19:30 -0400

    Add title and header for projectList component

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,9 +1,13 @@
 
 const ProjectList = () => {
   return (
-    <div>
-      <h1>My Projects</h1>
-    </div>
+    <section className="my-8">
+      <div className="flex justify-between w-11/12 m-auto items-center">
+        <div className="w-full h-2 shadow-md rounded-lg bg-mantis" />
+        <h1 className="text-shadow text-center w-2/3 px-4 font-bold text-2xl text-dark-spring-green" >My Projects</h1>
+        <div className="w-full h-2 shadow-md rounded-lg bg-mantis" />
+      </div>
+    </section>
   )
 }
 

8239f2d6cbd9c0e784593269a0d6caee60076b04
Luiscarlosvd <lc26cr7@gmail.com>
2023-10-16 17:22:07 -0400

    Add Images to Cards

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -4,7 +4,7 @@ const projects = [
   {
     id: 1,
     title: 'Luxury Speedsters 🚘',
-    photo: '#',
+    photo: 'https://lh3.google.com/u/1/d/1JYkre19MOxkk69W-_VaFNjiNyOVkcLFt=w1868-h903-iv1',
     description: `Experience the future of automotive luxury with our 
                   "Luxury Speedsters" app. This app enables users to browse 
                   a diverse catalog of cutting-edge vehicles, explore detailed 
@@ -17,12 +17,13 @@ const projects = [
   {
     id: 2,
     title: 'Luxury Speedsters 🚗',
-    photo: '#',
+    photo: 'https://lh3.google.com/u/1/d/13708KiJ8_1pn5IkeTJ2SZbp3TrtJA7l7=w2000-h3556-iv1',
     description: `Experience the future of automotive luxury with our 
                   "Luxury Speedsters" app. This app enables users to browse 
                   a diverse catalog of cutting-edge vehicles, explore detailed 
                   car profiles, and effortlessly create reservations for their dream cars. 
-                  Keep track of all your reservations in one place.`,
+                  Keep track of all your reservations in one place 
+                  .`,
     technologies: 'Ruby On Rails, React and Redux, Tailwind CSS, PostgreSQL',
     see_live: 'rentals-app-egry.onrender.com',
     see_source: 'https://github.com/Luiscarlosvd/futuristic-transportation/tree/main',
@@ -37,11 +38,13 @@ const ProjectList = () => {
         <h1 className="text-shadow text-center w-2/3 px-4 font-bold text-2xl text-dark-spring-green" >My Projects</h1>
         <div className="w-full h-2 shadow-md rounded-e-lg bg-mantis" />
       </div>
-      <div className="w-11/12 m-auto mt-12 flex flex-col gap-4">
+      <div className="w-11/12 m-auto mt-12 flex flex-col justify-center gap-4">
         {projects.map(project => (
           <ProjectCard
             key={project.id}
+            id={project.id}
             title={project.title}
+            photo={project.photo}
             description={project.description}
             technologies={project.technologies.split(", ")}
             linkSource={project.see_source}

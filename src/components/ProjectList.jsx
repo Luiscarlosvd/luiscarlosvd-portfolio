dbeb2bba6c48555452053324188f601e8d1e6900
Luiscarlosvd <lc26cr7@gmail.com>
2023-10-16 18:00:22 -0400

    Move projects Info to a external file

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,62 +1,6 @@
 import ProjectCard from "./projects/ProjectCard"
+import projects from "./projects/projectsInfo";
 import { useState } from "react"
-
-const projects = [
-  {
-    id: 1,
-    title: 'Luxury Speedsters 🚘',
-    photo: 'https://lh3.google.com/u/1/d/1JYkre19MOxkk69W-_VaFNjiNyOVkcLFt=w1868-h903-iv1',
-    description: `Experience the future of automotive luxury with our 
-                  "Luxury Speedsters" app. This app enables users to browse 
-                  a diverse catalog of cutting-edge vehicles, explore detailed 
-                  car profiles, and effortlessly create reservations for their dream cars. 
-                  Keep track of all your reservations in one place.`,
-    technologies: 'Ruby On Rails, React and Redux, Tailwind CSS, PostgreSQL',
-    see_live: 'rentals-app-egry.onrender.com',
-    see_source: 'https://github.com/Luiscarlosvd/futuristic-transportation/tree/main',
-  },
-  {
-    id: 2,
-    title: 'Luxury Speedsters 🚗',
-    photo: 'https://lh3.google.com/u/1/d/13708KiJ8_1pn5IkeTJ2SZbp3TrtJA7l7=w2000-h3556-iv1',
-    description: `Experience the future of automotive luxury with our 
-                  "Luxury Speedsters" app. This app enables users to browse 
-                  a diverse catalog of cutting-edge vehicles, explore detailed 
-                  car profiles, and effortlessly create reservations for their dream cars. 
-                  Keep track of all your reservations in one place 
-                  .`,
-    technologies: 'Ruby On Rails, React and Redux, Tailwind CSS, PostgreSQL',
-    see_live: 'rentals-app-egry.onrender.com',
-    see_source: 'https://github.com/Luiscarlosvd/futuristic-transportation/tree/main',
-  },
-  {
-    id: 3,
-    title: 'Luxury Speedsters 🚗',
-    photo: 'https://lh3.google.com/u/1/d/13708KiJ8_1pn5IkeTJ2SZbp3TrtJA7l7=w2000-h3556-iv1',
-    description: `Experience the future of automotive luxury with our 
-                  "Luxury Speedsters" app. This app enables users to browse 
-                  a diverse catalog of cutting-edge vehicles, explore detailed 
-                  car profiles, and effortlessly create reservations for their dream cars. 
-                  Keep track of all your reservations in one place 
-                  .`,
-    technologies: 'Ruby On Rails, React and Redux, Tailwind CSS, PostgreSQL',
-    see_live: 'rentals-app-egry.onrender.com',
-    see_source: 'https://github.com/Luiscarlosvd/futuristic-transportation/tree/main',
-  },
-  {
-    id: 4,
-    title: 'Luxury Speedsters 🚘',
-    photo: 'https://lh3.google.com/u/1/d/1JYkre19MOxkk69W-_VaFNjiNyOVkcLFt=w1868-h903-iv1',
-    description: `Experience the future of automotive luxury with our 
-                  "Luxury Speedsters" app. This app enables users to browse 
-                  a diverse catalog of cutting-edge vehicles, explore detailed 
-                  car profiles, and effortlessly create reservations for their dream cars. 
-                  Keep track of all your reservations in one place.`,
-    technologies: 'Ruby On Rails, React and Redux, Tailwind CSS, PostgreSQL',
-    see_live: 'rentals-app-egry.onrender.com',
-    see_source: 'https://github.com/Luiscarlosvd/futuristic-transportation/tree/main',
-  },
-]
 
 const ProjectList = () => {
   const [count, setCount] = useState(3);
